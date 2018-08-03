6942516d2c6032bd4595a20d81b13cec08562817
djcomlab <djcomlab@gmail.com>
2018-08-03 14:20:22 +0100

    Complete merge functionality for test case

--- mzml2isa_enhance.py
+++ mzml2isa_enhance.py
@@ -21,26 +21,25 @@ for assay in study.assays:
     # get mass spectrometry processes only
     ms_processes = [x for x in assay.process_sequence
                     if x.executes_protocol.protocol_type.term == 'mass spectrometry']
-    """
-    Check study.protocols for mass spectrometry incase ProtocolParameters
-    need to be added based on incoming new metadata from mzML
-    """
-
     # insert the new parameter values
     for k, v in mapping.items():
         with open(os.path.join('MTBLS265-no-binary', 'json_meta', v + '.json')) as fp2:
             mzml_meta = json.load(fp2)
-        try:
+            data_trans_meta = {k: v for k, v in mzml_meta.items() if
+                               k.startswith('Data Transformation')}
+            labels = {k: v for k, v in mzml_meta.items() if
+                k.endswith(('File', 'Name'))}
+            ms_prot_meta = {k: v for k, v in mzml_meta.items() if
+                            not k.startswith('Data Transformation') and
+                            k not in labels.keys()}
+        try:  #  add parameter values to MS process
             ms_process = [x for x in ms_processes if k in [y.filename for y in x.outputs]][0]
             pvs = ms_process.parameter_values
-            print('current pvs:', [x.category.parameter_name.term for x in pvs])
-            print('insert pvs here for ', ms_process.name)
-            for item in mzml_meta:
+            for item in ms_prot_meta:
                 if not ms_process.executes_protocol.get_param(item):
-                    print('need to add ', item, ' to protocol')
                     ms_process.executes_protocol.add_param(item)
                 param = ms_process.executes_protocol.get_param(item)
-                meta_item = mzml_meta[item]
+                meta_item = ms_prot_meta[item]
                 if 'value' in meta_item.keys():
                     value = meta_item['value']  # check for unit as well
                 elif 'name' in meta_item.keys():
@@ -57,13 +56,36 @@ for assay in study.assays:
                     raise IOError(meta_item)
                 pv = ParameterValue(category=param, value=value)
                 ms_process.parameter_values.append(pv)
-            """
-            Make sure to check if parameter to add exists, so that the pv is
-            updated rather than added to the pvs
-            """
-            print('new pvs:', [x.category.parameter_name.term for x in pvs])
+
+            #  set raw file name to mzML meta raw file name and sample name too
+            for output in ms_process.outputs:
+                if output.label in labels.keys():
+                    output.filename = labels[
+                    output.label]['entry_list'][-1]['value']
+                    output.generated_from[-1].name = labels[
+                    'Sample Name']['value']
+            #  set MS Assay Name to mzML metadata
+            ms_process.name = labels['MS Assay Name']['value']
+
+            #  add data transformation to describe conversion to mzML
+            if data_trans_meta['Data Transformation Name']:
+                if not study.get_prot('Conversion to mzML'):
+                    dt_prot = Protocol(name='Conversion to mzML',
+                        protocol_type=OntologyAnnotation(
+                        term='data transformation'))
+                    dt_prot.add_param('peak picking')
+                    dt_prot.add_param('software')
+                    dt_prot.add_param('software version')
+                    study.protocols.append(dt_prot)
+                dt_prot = study.get_prot('Conversion to mzML')
+                dt_process = Process(executes_protocol=dt_prot)
+                dt_process.outputs = [DerivedSpectralDataFile(
+                    filename=labels['Derived Spectral Data File'][
+                        'entry_list'][-1]['value'])]
+                dt_process.inputs = ms_process.outputs
+                plink(ms_process, dt_process)
+                assay.process_sequence.append(dt_process)
         except IndexError:
             pass
-
 
 isatab.dump(ISA, output_filepath)
